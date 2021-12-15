327ae208b9af2b63c78837d2e82646a93445baa6
Sarsenali007 <60970585+Sarsenali007@users.noreply.github.com>
2021-12-15 17:18:57 +0600

    Тест3

--- JavaAppiumAutomation/src/FirstTest.java
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -8,20 +8,21 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 
 public class FirstTest {
-    private AppiumDriver driver;
+
+    public AppiumDriver driver;
 
     @Before
     public void setUp() throws Exception{
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("platformVersion","6.0");
         capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.Wikipedia");
+        capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","/Users/sarsenalizhunisbek/Documents/GitHub/MobileAppium/apks/org.wikipedia.apk");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
     }
 
     @After
